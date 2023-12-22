afca103b3112fed1d8cbe5922c9574d0867ce019
aliaksandrsurma <aliaksandr.surma@gmail.com>
2023-12-22 15:11:03 +0300

    Updating configs for GitHub Pages deployments

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,18 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://aliaksandrsurma.github.io/cv-as-a-project-docs',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'aliaksandrsurma', // Usually your GitHub org/user name.
+  projectName: 'cv-as-a-project-docs', // Usually your repo name.
+
+  //recommended for GitHub Pages deployments by Docusaurus documentaton
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
